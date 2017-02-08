1a575147333c9530df0403a6fe304f05384af78f
Jhon efe <pc_systemjf@hotmail.com>
2017-02-08 11:29:28 -0500

    archivos css, js img
    se suben archivos css, js, img para el proyecto floristeria

---
+++ floristeriacolors/public/js/custom.js
@@ -0,0 +1,132 @@
+
+
+
+$(document).ready(function(){
+
+     $('#media').carousel({
+                pause: true,
+                interval: 5200,
+            });
+
+	/* ========================================================================= */
+	/*	Menu item highlighting
+	/* ========================================================================= */
+
+	jQuery('#nav').singlePageNav({
+		offset: jQuery('#nav').outerHeight(),
+		filter: ':not(.external)',
+		speed: 1200,
+		currentClass: 'current',
+		easing: 'easeInOutExpo',
+		updateHash: true,
+		beforeStart: function() {
+			console.log('begin scrolling');
+		},
+		onComplete: function() {
+			console.log('done scrolling');
+		}
+	});
+	
+	/* ========================================================================= */
+	/*	Fix Slider Height
+	/* ========================================================================= */	
+
+	var slideHeight = $(window).height();
+	
+	$('#slider, .carousel.slide, .carousel-inner, .carousel-inner .item').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('#slider, .carousel.slide, .carousel-inner, .carousel-inner .item').css('height',slideHeight);
+	});
+	
+	
+	/* ========================================================================= */
+	/*	Portfolio Filtering
+	/* ========================================================================= */	
+	
+	
+    var $container = $('.project-wrapper');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.work-filter a').click(function(){
+        $('.work-filter .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
+	
+	
+	$(".fancybox").fancybox({
+		padding: 0,
+
+		openEffect : 'elastic',
+		openSpeed  : 650,
+
+		closeEffect : 'elastic',
+		closeSpeed  : 550,
+
+		closeClick : true,
+	});
+	
+	/* ========================================================================= */
+	/*	Parallax
+	/* ========================================================================= */	
+	
+	$('#facts').parallax("50%", 0.5);
+	
+	/* ========================================================================= */
+	/*	Timer count
+	/* ========================================================================= */
+
+	"use strict";
+    $(".number-counters").appear(function () {
+        $(".number-counters [data-to]").each(function () {
+            var e = $(this).attr("data-to");
+            $(this).delay(6e3).countTo({
+                from: 50,
+                to: e,
+                speed: 3e3,
+                refreshInterval: 50
+            })
+        })
+    });
+	
+	/* ========================================================================= */
+	/*	Back to Top
+	/* ========================================================================= */
+	
+	
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 400) {
+            $("#back-top").fadeIn(200)
+        } else {
+            $("#back-top").fadeOut(200)
+        }
+    });
+    $("#back-top").click(function () {
+        $("html, body").stop().animate({
+            scrollTop: 0
+        }, 1500, "easeInOutExpo")
+    });
+	
+});
+
+
+
+
