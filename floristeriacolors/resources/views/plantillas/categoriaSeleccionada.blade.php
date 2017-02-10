ae5fd79f06b583a33d720788d33e73c5c0821acc
Jhon efe <pc_systemjf@hotmail.com>
2017-02-10 18:07:23 -0500

    se termina el archivo categoriaSeleccionada.blade.php
    se termina el archivo categoriaSeleccionada.blade.php se agrega un
    ultimo <section></section> donde se puede observar arreglos florales de
    interes para el cliente.

--- floristeriacolors/resources/views/plantillas/categoriaSeleccionada.blade.php
+++ floristeriacolors/resources/views/plantillas/categoriaSeleccionada.blade.php
@@ -146,11 +146,119 @@
 
                   <!-- fin arreglos de la respectiva categoria seleccionada -->
 
-
+                
                  
             </div>
             
         </div>
         
     </section>
+    <section class="div-descuento">
+        <div class="row">
+             <div class="col-md-12 text-center"> 
+                        <h2>
+                            Otros clientes también estan Buscando Arreglos Florales.
+                        </h2>
+                        <p>
+                            Aprovecha ahora y separa tu regálo.!
+                        </p>
+                      
+            </div>
+            
+        </div>
+        
+    </section>
+    <div class="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- un arreglo -->
+                            <div class="col-md-2 wow fadeInLeft" data-wow-duration="500ms">
+                                <div class="service-item">
+                                                            
+                                    <div class="service-desc">
+                                        
+                                       
+                                         <img src="img/arreglos/AMANECER2.jpg" class="img-responsive">
+                                          <h3>Rosas!</h3>
+                                          <h3>COP 40.000</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- fin un arreglo -->
+                            <!-- un arreglo -->
+                            <div class="col-md-2 wow fadeInLeft" data-wow-duration="500ms">
+                                <div class="service-item">
+                                                            
+                                    <div class="service-desc">
+                                        
+                                       
+                                         <img src="img/arreglos/cajarosas.jpg" class="img-responsive">
+                                          <h3>Rosas!</h3>
+                                          <h3>COP 40.000</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- fin un arreglo -->
+                            <!-- un arreglo -->
+                            <div class="col-md-2 wow fadeInLeft" data-wow-duration="500ms">
+                                <div class="service-item">
+                                                            
+                                    <div class="service-desc">
+                                        
+                                       
+                                         <img src="img/arreglos/mi corazon.jpg" class="img-responsive">
+                                          <h3>Rosas!</h3>
+                                          <h3>COP 40.000</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- fin un arreglo -->
+                            <!-- un arreglo -->
+                            <div class="col-md-2 wow fadeInLeft" data-wow-duration="500ms">
+                                <div class="service-item">
+                                                            
+                                    <div class="service-desc">
+                                        
+                                       
+                                         <img src="img/arreglos/jarron-impresionista.jpg" class="img-responsive">
+                                          <h3>Rosas!</h3>
+                                          <h3>COP 40.000</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- fin un arreglo -->
+                            <!-- un arreglo -->
+                            <div class="col-md-2 wow fadeInLeft" data-wow-duration="500ms">
+                                <div class="service-item">
+                                                            
+                                    <div class="service-desc">
+                                        
+                                       
+                                         <img src="img/arreglos/alegria.jpg" class="img-responsive">
+                                          <h3>Rosas!</h3>
+                                          <h3>COP 40.000</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- fin un arreglo -->
+                            <!-- un arreglo -->
+                            <div class="col-md-2 wow fadeInLeft" data-wow-duration="500ms">
+                                <div class="service-item">
+                                                            
+                                    <div class="service-desc">
+                                        
+                                       
+                                         <img src="img/arreglos/MI-DETALLE.jpg" class="img-responsive">
+                                          <h3>Rosas!</h3>
+                                          <h3>COP 40.000</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- fin un arreglo -->
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
