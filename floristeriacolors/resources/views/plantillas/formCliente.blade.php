7520c2b9c59b3b7b4c697653ce58aa6337bea874
jorgeluis91x <jorge_luis91x@hotmail.com>
2017-02-28 22:45:10 -0500

    prueba clientes migracion

--- floristeriacolors/resources/views/plantillas/formCliente.blade.php
+++ floristeriacolors/resources/views/plantillas/formCliente.blade.php
@@ -7,7 +7,7 @@
                         {!!Form::date('fecha_nacimiento',null,['class'=>'form-control','placeholder'=>'Fecha de Nacimiento'])!!}
                         {!!Form::text('celular',null,['class'=>'form-control','placeholder'=>'Celular'])!!}
                         {!!Form::text('direccion',null,['class'=>'form-control','placeholder'=>'Direccion'])!!}
-                        {!!Form::hidden('user_id',null)!!}
+                        {!!Form::hidden('user_id',{{$user->id}})!!}
 
                        <!--- <select class="form-control se">
                             <option>Ciudad</option>
