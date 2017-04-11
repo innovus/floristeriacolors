592eb80f78a58a73737368cd4072c3c950988820
Jhon efe <pc_systemjf@hotmail.com>
2017-04-11 12:16:34 -0500

    cambios en listado de ventas
    listado de venta en el admin

--- floristeriacolors/public/assets/js/demop.js
+++ floristeriacolors/public/assets/js/demop.js
@@ -1,20 +1,22 @@
 function verbtn(n) {
 	
-	$('#btn-ver'+n).click(function() {
+		alert(n);
+		/*---------------------------------------*/
 		
 		type = $(this).attr('data-type');
 		
-		$('.overlay-container').fadeIn(function() {
+		$('#o'+n).fadeIn(function() {
 			
 			window.setTimeout(function(){
-				$('.window-container.'+type).addClass('window-container-visible');
+				$("#w"+n).addClass('window-container-visible');
 			}, 100);
 			
 		});
-	});
+		/*---------------------------------------*/
 	
-	$('#close').click(function() {
+	$('#close'+n).click(function() {
 		$('.overlay-container').fadeOut().end().find('.window-container').removeClass('window-container-visible');
 	});
 	
 }
+/*-----------------------------------------------------------------------*/
--- floristeriacolors/resources/views/plantillas/ventas.blade.php
+++ floristeriacolors/resources/views/plantillas/ventas.blade.php
@@ -45,18 +45,7 @@
                         <td><a href="#info3" data-type="zoomin" id="btn-ver2" onclick="verbtn(2)" class="btn btn-success">ver</a></td>
                     </tr>
                    
-                <!-- finr un movimiento -->  
-                <!--inicio un movimiento-->
-                    <tr>
-                        <td>10/04/2017</td>
-                        <td>15</td>
-                         <td>Pedro Acosta</td>
-                        <td>23000</td>
-                        <td>atendido</td>
-                        <td><a href="#info2" data-type="zoomin" id="btn-ver3" onclick="verbtn(3)" class="btn btn-danger">ver</a></td>
-                    </tr>
-                   
-                <!-- finr un movimiento -->              
+                <!-- finr un movimiento -->               
                 
                 </tbody>
             </table>
@@ -64,13 +53,184 @@
         </div>
     </div>
 </div>
-
-<div class="overlay-container">
-        <div class="window-container zoomin card">
-            <span class="fa fa-times-circle-o fa-2x" id="close"></span><br>
+<!-- un carrito-->
+<div class="overlay-container" id="o1">
+        <div class="window-container zoomin card" id="w1">
+            <span class="fa fa-times-circle-o fa-2x" id="close1"></span><br>
         <div class="pp">
             <div class="header text-center">
-                    <h4 class="title">CARRITO DE COMPRAS # 12</h4>
+                    <h4 class="title">CARRITO DE COMPRAS # 1 </h4>
+                </div>  
+            <hr>
+            <!-- ****************************************************** -->
+            <div class="col-md-8 col-md-offset-2 text-left">
+                  <!-- un producto -->
+                    <div class="row">
+                        <div class="content table-responsive table-full-width">
+                            <table class="table table-hover table-striped">
+                                <thead>
+                                    <th>Imagen</th>
+                                    <th>Categoria</th>
+                                    <th>Nombre</th>
+                                    <th>Valor</th>
+                                   
+                                </thead>
+                                <tbody>
+                                <!--inicio un movimiento-->
+                                    <tr>
+                                        <td>
+                                            <img class="img-responsive" style="width: 50px;" src="/img/arreglos/10arreglo final 1.jpg">
+                                        </td>
+                                        <td>Rosas</td>
+                                        <td>Caja de Rosas</td>
+                                        <td>23000</td>
+                                    </tr>
+                                   
+                                <!-- finr un movimiento --> 
+                                
+                                <!--inicio un movimiento-->
+                                    <tr>
+                                        <td>
+                                            <img class="img-responsive" style="width: 50px;" src="/img/arreglos/10arreglo final 1.jpg">
+                                        </td>
+                                        <td>Rosas</td>
+                                        <td>Caja de Rosas</td>
+                                        <td>23000</td>
+                                    </tr>
+                                   
+                                <!-- finr un movimiento -->  
+                                       
+                                
+                                </tbody>
+                            </table>
+
+                        </div>
+                        <!--DETALLES DEL COMPRADOR -->
+                        <hr>
+                        <div class="row">
+                        <div class="col-md-12"><label>DATOS COMPRADOR</label></div>
+                            <!-- UN DATO-->
+                            <div class="col-md-12">
+                                <div class="col-md-6">
+                                    <label>NOMBRES Y APELLIDOS:</label>
+                                </div>
+                                <div class="col-md-6">
+                                    JHON FREY DIAZ D
+                                </div>
+                            </div>
+                            <!--FIN UN DATO -->
+                            <!-- UN DATO-->
+                            <div class="col-md-12">
+                                <div class="col-md-6">
+                                    <label>CÉDULA:</label>
+                                </div>
+                                <div class="col-md-6">
+                                    1122783249
+                                </div>
+                            </div>
+                            <!--FIN UN DATO -->
+                            <!-- UN DATO-->
+                            <div class="col-md-12">
+                                <div class="col-md-6">
+                                    <label>TELÉFONO:</label>
+                                </div>
+                                <div class="col-md-6">
+                                    3173187766
+                                </div>
+                            </div>
+                            <!--FIN UN DATO -->
+                        </div>
+                        <!--FIN DETALLES DEL COMPRADOR -->
+                        <!--DETALLES DEL DESTINATARIO -->
+                        <hr>
+                        <div class="row">
+                        <div class="col-md-12"><label>DATOS DESTINATARIO</label></div>
+                            <!-- UN DATO-->
+                            <div class="col-md-12">
+                                <div class="col-md-6">
+                                    <label>NOMBRE Y APELLIDOS:</label>
+                                </div>
+                                <div class="col-md-6">
+                                    ESTEFANIA SANTACRUZ 
+                                </div>
+                            </div>
+                            <!--FIN UN DATO -->
+                            <!-- UN DATO-->
+                            <div class="col-md-12">
+                                <div class="col-md-6">
+                                    <label>DIRECCIÓN:</label>
+                                </div>
+                                <div class="col-md-6">
+                                    LAS CUADRAS
+                                </div>
+                            </div>
+                            <!--FIN UN DATO -->
+                            <!-- UN DATO-->
+                            <div class="col-md-12">
+                                <div class="col-md-6">
+                                    <label>TELÉFONO:</label>
+                                </div>
+                                <div class="col-md-6">
+                                    3173187766
+                                </div>
+                            </div>
+                            <!--FIN UN DATO -->
+                            <!-- UN DATO-->
+                            <div class="col-md-12">
+                                <div class="col-md-6">
+                                    <label>FECHA DE ENTREGA:</label>
+                                </div>
+                                <div class="col-md-6">
+                                    10/4/2017
+                                </div>
+                            </div>
+                            <!--FIN UN DATO -->
+                            <!-- UN DATO-->
+                            <div class="col-md-12">
+                                <div class="col-md-4">
+                                    <label>MENSAJE TARJETA:</label>
+                                </div>
+                                <div class="col-md-8">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, cupiditate sequi velit. Obcaecati excepturi quibusdam deleniti commodi eveniet in hic sed tenetur nemo laudantium labore omnis amet, dolorum, impedit, eos!
+                                </div>
+                            </div>
+                            <!--FIN UN DATO -->
+                            <!-- UN DATO-->
+                            <div class="col-md-12">
+                                <div class="col-md-4">
+                                    <label>OBSERVACIÓN:</label>
+                                </div>
+                                <div class="col-md-8">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus deserunt corporis, illo vitae, quasi reiciendis aspernatur sed cum officia eius, eum accusantium similique. Non molestias et asperiores libero, consectetur necessitatibus.
+                                </div>
+                            </div>
+                            <!--FIN UN DATO -->
+                        </div>
+                        <!--FIN DETALLES DEL DESTINATARIO -->
+                        <div class="col-md-9 col-md-offset-2 text-center">
+                            <h4><strong>TOTAL:1540000</strong></h4>
+                        </div>
+                        <div class="col-md-9 col-md-offset-2 text-center">
+                            <a href=""  class="btn btn-danger">CONFIRMAR COMPRA</a>
+                        </div>
+                    </div> 
+                <!-- fin un producto --> 
+            </div>     
+            <!--******************************************************* -->
+        </div>
+                
+            
+        </div>
+        
+</div>
+<!-- fin un carrito  -->
+<!-- un carrito-->
+<div class="overlay-container" id="o2">
+        <div class="window-container zoomin card" id="w2">
+            <span class="fa fa-times-circle-o fa-2x" id="close2"></span><br>
+        <div class="pp">
+            <div class="header text-center">
+                    <h4 class="title">CARRITO DE COMPRAS # 13</h4>
                 </div>  
             <hr>
             <!-- ****************************************************** -->
@@ -266,7 +426,10 @@
             
         </div>
         
-    </div>
+</div>
+<!-- fin un carrito  -->
+
+<!-- ******************************************************************* -->
 <script>!window.jQuery && document.write(unescape('%3Cscript src="jquery-1.7.1.min.js"%3E%3C/script%3E'))</script>
     <script type="text/javascript" src="/assets/js/demo.js"></script>
 @stop
